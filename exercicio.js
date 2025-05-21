4a7b3b021affb69b020e80151c2f816dfb6b5a3f
Rivaldo <149204054+orivaldomoura@users.noreply.github.com>
2025-05-21 20:06:41 -0300

    Add files via upload

---
+++ exercicio.js
@@ -0,0 +1,12 @@
+"use strict";
+// função 1 de multiplicação
+function multiplicar(a, b) {
+    return a * b;
+}
+const resultado = multiplicar(4, 5);
+// função 2 de saudação
+function saudacao(nome) {
+    return `Olá, ${nome}! Seja bem-vindo.`;
+}
+const mensagem = saudacao("Evellyn");
+console.log(mensagem);
---
+++ exercicio.ts
@@ -0,0 +1,14 @@
+// função 1 de multiplicação
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+const resultado = multiplicar(4, 5);
+
+// função 2 de saudação
+function saudacao(nome: string): string {
+    return `Olá, ${nome}! Seja bem-vindo.`;
+}
+
+const mensagem = saudacao("Evellyn");
+console.log(mensagem);
